b38937be37d6e75e59da6d2e191ed5b12791c49f
SudiptaSengupta <SudiptaSengupta@192.168.0.4>
2026-08-04 17:09:36 +0530

    Added first module

---
+++ ai-generated-tweet-to-kafka-service/src/main/java/com/ssg/ai/tweet/AiGeneratedTweetToKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ssg.ai.tweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGeneratedTweetToKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiGeneratedTweetToKafkaServiceApplication.class, args);
+	}
+
+}
---
+++ ai-generated-tweet-to-kafka-service/src/test/java/com/ssg/ai/tweet/AiGeneratedTweetToKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssg.ai.tweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGeneratedTweetToKafkaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
