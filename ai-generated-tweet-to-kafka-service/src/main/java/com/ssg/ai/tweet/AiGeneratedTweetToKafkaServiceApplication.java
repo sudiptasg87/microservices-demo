33bb17345b1c189e2fd754d3c4aa8c3b4e8a5b0f
SudiptaSengupta <SudiptaSengupta@192.168.0.4>
2026-08-11 16:50:25 +0530

    Recent updates

--- ai-generated-tweet-to-kafka-service/src/main/java/com/ssg/ai/tweet/AiGeneratedTweetToKafkaServiceApplication.java
+++ ai-generated-tweet-to-kafka-service/src/main/java/com/ssg/ai/tweet/AiGeneratedTweetToKafkaServiceApplication.java
@@ -2,8 +2,10 @@ package com.ssg.ai.tweet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages =  "com.ssg.ai")
 public class AiGeneratedTweetToKafkaServiceApplication {
 
 	public static void main(String[] args) {
